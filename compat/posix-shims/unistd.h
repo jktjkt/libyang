dfc8a3c45034ee89e29150c555e6578beed9c475
Jan Kundrát <jan.kundrat@cesnet.cz>
2021-12-09 23:31:41 +0100

    unistd.h

--- compat/posix-shims/unistd.h
+++ compat/posix-shims/unistd.h
@@ -34,6 +34,12 @@
 #define lseek _lseek
 /* read, write, and close are NOT being #defined here, because while there are file handle specific versions for Windows, they probably don't work for sockets. You need to look at your app and consider whether to call e.g. closesocket(). */
 
+#ifdef _WIN64
+#define ssize_t __int64
+#else
+#define ssize_t long
+#endif
+
 #define STDIN_FILENO 0
 #define STDOUT_FILENO 1
 #define STDERR_FILENO 2
