952b737e68705e63feaedd81efeeee5384f78851
Jan Kundrát <jan.kundrat@cesnet.cz>
2021-12-11 02:16:27 +0100

    MSVC: tools: disable interactive tools
    I don't feel like porting linenoise to Windows, there's no getline, etc.

---
+++ tools/lint/main_ni_only.c
@@ -0,0 +1,23 @@
+/**
+ * @file main.c
+ * @author Radek Krejci <rkrejci@cesnet.cz>
+ * @brief libyang's yanglint tool
+ *
+ * Copyright (c) 2015-2020 CESNET, z.s.p.o.
+ *
+ * This source code is licensed under BSD 3-Clause License (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/BSD-3-Clause
+ */
+
+int main_ni(int argc, char *argv[]);
+
+int done; /* for cmd.c */
+
+int
+main(int argc, char *argv[])
+{
+    return main_ni(argc, argv);
+}
