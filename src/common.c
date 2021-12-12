6b9598d6917ced9ca7cc11a1edef5dbe702fcacd
Jan Kundrát <jan.kundrat@cesnet.cz>
2021-12-12 03:12:23 +0100

    MSVC: there's no mmap() on Windows
    There's a compatibility library, the mman-win32, which implement a
    mmap-like semantics. However, on Windows one cannot map a read-only file
    into memory for a length that is larger than the size of that file,
    and you also cannot overwrite an existing mapping.
    
    The easiest course of action is to just read the file into memory and be
    done with it.
    
    See-also: https://stackoverflow.com/a/46014637/2245623

--- src/common.c
+++ src/common.c
@@ -24,7 +24,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#ifndef _WIN32
 #include <sys/mman.h>
+#endif
 #include <sys/stat.h>
 #include <unistd.h>
 
@@ -369,6 +371,7 @@ ly_mmap(struct ly_ctx *ctx, int fd, size_t *length, void **addr)
         *addr = NULL;
         return LY_SUCCESS;
     }
+#ifndef _WIN32
     pagesize = sysconf(_SC_PAGESIZE);
 
     m = sb.st_size % pagesize;
@@ -391,16 +394,48 @@ ly_mmap(struct ly_ctx *ctx, int fd, size_t *length, void **addr)
         LOGERR(ctx, LY_ESYS, "mmap() failed (%s).", strerror(errno));
         return LY_ESYS;
     }
+#else
+    /* On Windows, the mman-win32 mmap() emulation uses CreateFileMapping and MapViewOfFile, and these functions
+     * do not allow mapping more than "length of file" bytes for PROT_READ. Remapping existing mappings is not allowed, either.
+     * At that point the path of least reisstance is just reading the file in as-is. */
+    m = sb.st_size + 1;
+    char *buf = calloc(m, 1);
+    if (!buf) {
+        LOGERR(ctx, LY_ESYS, "ly_mmap: malloc() failed (%s).", strerror(errno));
+    }
+    *addr = buf;
+    *length = m;
 
+    lseek(fd, 0, SEEK_SET);
+    ssize_t to_read = m - 1;
+    while (to_read > 0) {
+        ssize_t n = read(fd, buf, to_read);
+        if (n == 0) {
+            return LY_SUCCESS;
+        } else if (n < 0) {
+            if (errno == EINTR) {
+                continue; // can I get this on Windows?
+            }
+            LOGERR(ctx, LY_ESYS, "ly_mmap: read() failed (%s).", strerror(errno));
+        }
+        to_read -= n;
+        buf += n;
+    }
+#endif
     return LY_SUCCESS;
 }
 
 LY_ERR
 ly_munmap(void *addr, size_t length)
 {
+#ifndef _WIN32
     if (munmap(addr, length)) {
         return LY_ESYS;
     }
+#else
+    (void)length;
+    free(addr);
+#endif
     return LY_SUCCESS;
 }
 
